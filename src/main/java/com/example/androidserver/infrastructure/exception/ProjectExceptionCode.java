799799065bd1b1e2b04279c4500eadcb3ddf9351
namung08 <namung08@gmail.com>
2024-10-11 17:48:14 +0900

    :goal_net: perf(exception): 에러 코드 추가

--- src/main/java/com/example/androidserver/infrastructure/exception/ProjectExceptionCode.java
+++ src/main/java/com/example/androidserver/infrastructure/exception/ProjectExceptionCode.java
@@ -8,7 +8,7 @@ import lombok.Getter;
 public enum ProjectExceptionCode {
   // Common 한글로 변경 해두 됨
   INVALID_INPUT_VALUE("C001", "Invalid Input Value"),
-  ;
+  NOT_FOUND_USER("C002", "사용자를 찾을 수 없습니다."),;
   private final String code;
   private final String message;
 }
