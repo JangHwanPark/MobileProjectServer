0c67ef4094f258a4add64d27a19a83bacd6c73fd
namung08 <namung08@gmail.com>
2024-10-11 17:59:17 +0900

    :zap: perf(model): 일부 필드 변경 및 추가

--- src/main/java/com/example/androidserver/user/model/User.java
+++ src/main/java/com/example/androidserver/user/model/User.java
@@ -1,14 +1,27 @@
 package com.example.androidserver.user.model;
 
-import lombok.Getter;
-import lombok.Setter;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import jakarta.persistence.Table;
+import lombok.*;
+import org.springframework.data.annotation.Id;
 
+@Table(name = "user")
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class User {
-    private int uid;
+    @Id
+    private Long id;
+    // udid 값은 기기 고유 값인데 아마 string 일 거임
+    private String udid;
+    // 이건 알아서 하셈
     private int rid;
     private String email;
     private String password;
+    private String role;
+    // 관심?
     private String interest;
 }
