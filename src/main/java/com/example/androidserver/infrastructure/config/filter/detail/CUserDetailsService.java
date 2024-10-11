6fb7fefc7973507eb9b161f4cf601221c9afd620
namung08 <namung08@gmail.com>
2024-10-11 18:10:09 +0900

    :zap: perf: repo 에서 service 연결로 변경

--- src/main/java/com/example/androidserver/infrastructure/config/filter/detail/CUserDetailsService.java
+++ src/main/java/com/example/androidserver/infrastructure/config/filter/detail/CUserDetailsService.java
@@ -1,11 +1,8 @@
 package com.example.androidserver.infrastructure.config.filter.detail;
 
-import com.example.androidserver.infrastructure.exception.ProjectException;
-import com.example.androidserver.infrastructure.exception.ProjectExceptionCode;
 import com.example.androidserver.user.model.User;
-import com.example.androidserver.user.repo.UserRepository;
+import com.example.androidserver.user.service.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -14,11 +11,10 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class CUserDetailsService implements UserDetailsService {
-  private final UserRepository repository;
+  private final UserService service;
   @Override
   public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-    User user = repository.findByEmail(username)
-            .orElseThrow(() -> new ProjectException(HttpStatus.NOT_FOUND, ProjectExceptionCode.NOT_FOUND_USER));
+    User user = service.getUserByEmail(username);
     return org.springframework.security.core.userdetails.User.builder()
             .username(user.getEmail())
             .password(user.getPassword())
