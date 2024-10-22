a5252845b55caa7af07c678ebc3f630292bad2d2
namung08 <namung08@gmail.com>
2024-10-22 10:44:13 +0900

    :zap: perf: QuestionController으로 요청을 보낼 때 시작 주소 설정
     `@RequestMapping` 을 통해 컨트롤러에 선언이 되어 있는 모든 요청에 대해서 end-point 의 시작을 설정하였습니다.

--- src/main/java/com/example/androidserver/web/Controller/QuestionController.java
+++ src/main/java/com/example/androidserver/web/Controller/QuestionController.java
@@ -4,10 +4,7 @@ import com.example.androidserver.Question.model.Question;
 import com.example.androidserver.Question.service.QuestionService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.sql.CallableStatement;
 import java.sql.Connection;
@@ -16,6 +13,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
+// 이 컨트롤러의 모든 요청은 /question으로 시작
+@RequestMapping("/question")
 // @autowired 와 생성자를 대체하는 어노테이션
 @RequiredArgsConstructor
 public class QuestionController {
