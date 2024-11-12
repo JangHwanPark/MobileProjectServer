7c851bdc37288cd5625ba00993357dc150bcff72
user <wkd5878@gmail.com>
2024-11-12 23:26:59 +0900

    :sparkle: feat - 테이블 생성 쿼리 sql 파일 추가, procedure, insert 로직 업데이트

---
+++ src/main/sql/db_create_table.sql
@@ -0,0 +1,52 @@
+create table user
+(
+    uid      int auto_increment primary key,
+    name     varchar(10),
+    email    varchar(50) unique,
+    password varchar(255),
+    interest varchar(100),
+    company  varchar(100),
+    role     varchar(10),
+    birth    varchar(20)
+);
+
+create table question
+(
+    qid      int auto_increment primary key,
+    uid      int,
+    interest varchar(100),
+    title    varchar(255),
+    content  varchar(255),
+    category varchar(50),
+    createAt date,
+    updateAt date,
+    great    int,
+    foreign key (uid) references user (uid)
+);
+
+create table comment
+(
+    cid      int auto_increment primary key,
+    uid      int,
+    qid      int,
+    content  varchar(255),
+    createAt date,
+    updateAt date,
+    foreign key (uid) references user (uid),
+    foreign key (qid) references question (qid)
+);
+
+create table tag
+(
+    tid   int primary key,
+    tname varchar(30)
+);
+
+create table question_tag
+(
+    qid int,
+    tid int,
+    primary key (tid, qid),
+    foreign key (qid) references question (qid) on delete cascade,
+    foreign key (tid) references tag (tid)
+);
--- src/main/sql/db_procedure.sql
+++ src/main/sql/db_procedure.sql
@@ -13,7 +13,6 @@ create index idx_comment_qid on comment (qid);
 -- 코멘트 테이블에서 사용자 ID(uid)로 조회 성능 향상을 위한 인덱스
 create index idx_comment_uid on comment (uid);
 
-
 -- View
 drop view if exists question_with_user;
 create view question_with_user as
@@ -47,14 +46,14 @@ from comment as c
 drop view if exists question_with_comment;
 create view question_with_comment as
 select q.qid,
-       q.uid as question_uid,
-       q.content as question_content,
+       q.uid      as question_uid,
+       q.content  as question_content,
        q.category,
        q.title,
        q.createAt as question_createAt,
        c.createAt as comment_createAt
 from question as q
-join comment as c on q.qid = c.qid;
+         join comment as c on q.qid = c.qid;
 
 
 -- Trigger
@@ -68,7 +67,6 @@ begin
     delete from comment where qid = OLD.qid;
 end;
 
-
 -- Procedure
 -- 동적 테이블 조회
 drop procedure if exists select_all_data;
@@ -76,11 +74,10 @@ delimiter
 //
 create procedure select_all_data(in tableName varchar(50))
 begin
-    set
-        @query = concat('select * from ', tableName);
-    prepare stmt from @query;
-    execute stmt;
-    deallocate prepare stmt;
+    set @query = concat('select * from ', tableName);
+prepare stmt from @query;
+execute stmt;
+deallocate prepare stmt;
 end
 //
 delimiter ;
@@ -100,8 +97,8 @@ create procedure create_user(
     in p_company varchar(100)
 )
 begin
-    insert into user (name, email, password, interest, company, role, birth)
-    values (p_name, p_email, p_password, p_interest, p_company, p_role, p_birth);
+insert into user (name, email, password, interest, company, role, birth)
+values (p_name, p_email, p_password, p_interest, p_company, p_role, p_birth);
 end
 //
 delimiter ;
@@ -114,9 +111,9 @@ create procedure select_uid_by_email(
     in p_email varchar(50)
 )
 begin
-    select uid
-    from user
-    where email = p_email;
+select uid
+from user
+where email = p_email;
 end
 //
 delimiter ;
@@ -136,8 +133,8 @@ CREATE PROCEDURE create_question(
     IN p_updateAt TIMESTAMP
 )
 BEGIN
-    INSERT INTO question (qid, uid, content, category, title, createAt, updateAt, great)
-    VALUES (p_qid, p_uid, p_content, p_category, p_title, p_createAt, p_updateAt, 0);
+INSERT INTO question (qid, uid, content, category, title, createAt, updateAt, great)
+VALUES (p_qid, p_uid, p_content, p_category, p_title, p_createAt, p_updateAt, 0);
 END
 //
 DELIMITER ;
@@ -154,12 +151,12 @@ create procedure update_question(
     IN p_updateAt TIMESTAMP
 )
 begin
-    UPDATE question
-    SET content  = p_content,
-        category = p_category,
-        title    = p_title,
-        updateAt = p_updateAt
-    WHERE qid = p_qid;
+UPDATE question
+SET content  = p_content,
+    category = p_category,
+    title    = p_title,
+    updateAt = p_updateAt
+WHERE qid = p_qid;
 end
 //
 delimiter ;
@@ -168,13 +165,11 @@ delimiter ;
 drop procedure if exists delete_question;
 delimiter
 //
-create procedure delete_question(
-    in p_qid int
-)
+create procedure delete_question(in p_qid int)
 begin
-    delete
-    from question
-    where qid = p_qid;
+delete
+from question
+where qid = p_qid;
 end
 //
 delimiter ;
@@ -187,10 +182,10 @@ create procedure select_question_by_category(
     in p_category varchar(10)
 )
 begin
-    select *
-    from question_with_user
-    where category = p_category
-    group by qid;
+select *
+from question_with_user
+where category = p_category
+group by qid;
 end
 //
 delimiter ;
@@ -199,13 +194,11 @@ delimiter ;
 drop procedure if exists select_my_question;
 delimiter
 //
-create procedure select_my_question(
-    in p_uid int
-)
+create procedure select_my_question(in p_uid int)
 begin
-    select *
-    from question_with_user
-    where uid = p_uid;
+select *
+from question_with_user
+where uid = p_uid;
 end
 //
 delimiter ;
@@ -218,9 +211,9 @@ create procedure select_question_by_keyword(
     in p_keyword varchar(255)
 )
 begin
-    select *
-    from question
-    where title like concat('%', p_keyword, '%');
+select *
+from question
+where title like concat('%', p_keyword, '%');
 end
 //
 delimiter ;
@@ -234,9 +227,9 @@ create procedure increment_great(
     in p_qid int
 )
 begin
-    update question
-    set great = great + 1
-    where qid = p_qid;
+update question
+set great = great + 1
+where qid = p_qid;
 end
 //
 delimiter ;
@@ -245,13 +238,11 @@ delimiter ;
 drop procedure if exists select_great_count;
 delimiter
 //
-create procedure select_great_count(
-    in p_qid int
-)
+create procedure select_great_count(in p_qid int)
 begin
-    select great
-    from question
-    where qid = p_qid;
+select great
+from question
+where qid = p_qid;
 end
 //
 delimiter ;
@@ -270,8 +261,8 @@ create procedure create_comment(
     IN p_updateAt TIMESTAMP
 )
 begin
-    insert into comment (cid, uid, qid, content, createAt, updateAt)
-    values (p_cid, p_uid, p_qid, p_content, p_createAt, p_updateAt);
+insert into comment (cid, uid, qid, content, createAt, updateAt)
+values (p_cid, p_uid, p_qid, p_content, p_createAt, p_updateAt);
 end
 //
 delimiter ;
@@ -286,10 +277,10 @@ create procedure update_comment(
     in p_updateAt date
 )
 begin
-    update comment
-    set content  = p_content,
-        updateAt = p_updateAt
-    where cid = p_cid;
+update comment
+set content  = p_content,
+    updateAt = p_updateAt
+where cid = p_cid;
 end
 //
 delimiter ;
@@ -298,42 +289,22 @@ delimiter ;
 drop procedure if exists select_comment_by_question_id;
 delimiter
 //
-create procedure select_comment_by_question_id(
-    in p_qid int
-)
+create procedure select_comment_by_question_id(in p_qid int)
 begin
-    select *
-    from comment_with_user
-    where qid = p_qid
-    group by cid;
-end
-//
+select *
+from comment_with_user
+where qid = p_qid
+group by cid;
+end //
 delimiter ;
 
--- 코멘트 삭제 (동적 쿼리)
-drop procedure if exists delete_comment_condition_integer;
-delimiter
-//
-create procedure delete_comment_condition_integer(
-    in p_column_name varchar(50),
-    in p_value int
-)
+-- 코멘트 삭제
+drop procedure if exists delete_comment_by_id;
+delimiter //
+create procedure delete_comment_by_id(in p_cid int)
 begin
-    -- 미리 정의된 안전한 컬럼 이름 목록
-    if
-        p_column_name not in ('cid', 'qid') then
-        signal sqlstate '45000' set message_text = 'Invalid column name';
-    end if;
-
-    set
-        @value = p_value;
-    set
-        @query = concat('delete from comment where', p_column_name, '= ?');
-    prepare stmt from @query;
-    execute stmt using @value;
-    deallocate prepare stmt;
-end
-//
+delete from comment where cid = p_cid;
+end //
 delimiter ;
 
 -- 전체 코멘트 삭제
@@ -342,9 +313,7 @@ delimiter
 //
 create procedure delete_comment(in p_qid int)
 begin
-    delete
-    from comment
-    where qid = p_qid;
+delete from comment where qid = p_qid;
 end
 //
 delimiter ;
@@ -364,34 +333,110 @@ begin
                 when p_period = 'monthly' then date_sub(curdate(), interval 1 month)
                 when p_period = 'yearly' then date_sub(curdate(), interval 1 year)
                 else curdate()
-                end;
+end;
+
+select q.uid, u.name, u.email, u.company, count(*) as question_count
+from question as q
+         join user as u on q.uid = u.uid
+where createAt >= start_date
+group by q.uid;
 end //
 delimiter ;
 
--- 사용자가 작성한 질문의 개수
-drop procedure if exists get_user_question_count;
+-- 사용자가 작성한 코멘트 or 질문의 개수
+drop procedure if exists get_written_count;
 delimiter //
-create procedure get_user_question_count(in p_uid int, out question_count int)
+create procedure get_written_count(
+    in p_uName varchar(10),
+    in p_tableName varchar(20)
+)
 begin
-    select count(*)
-    into question_count
-    from question
-    where uid = p_uid;
+    set @query = concat(
+            'select u.uid, u.name, u.email, count(*) as ', p_uName, '_cnt '
+                'from ', p_tableName, ' as tname '
+                'inner join user as u on u.uid = tname.uid ',
+            'where u.name = ''', p_uName, ''' ',
+            'group by u.uid');
+
+prepare stmt from @query;
+execute stmt;
+deallocate prepare stmt;
 end //
 delimiter ;
 
-SET @question_count = 0;
-CALL get_user_question_count(2, @question_count);
-SELECT @question_count;
-
--- 사용자가 작성한 코멘트 개수
-drop procedure if exists get_user_comment_count;
+-- 사용자별 평균 질문 or 코멘트 개수
+drop procedure if exists get_average_count;
 delimiter //
-create procedure get_user_comment_count(in p_uid int)
+create procedure get_average_count(
+    in p_tName varchar(20)
+)
 begin
-    select count(*)
-    from comment
-    where uid = p_uid;
+    set @query = concat(
+            'select u.uid, u.name, u.email, avg(user_counts.cnt) as average_', p_tName, '_count ',
+            'from user as u ',
+            'join (',
+            '   select uid, count(*) as cnt ',
+            '   from ', p_tName, ' as t ',
+            '   group by uid',
+            ') as user_counts on u.uid = user_counts.uid ',
+            'group by u.uid'
+                 );
+
+prepare stmt from @query;
+execute stmt;
+deallocate prepare stmt;
+end //
+delimiter ;
+
+-- 질문, 코멘트를 입력받아 가장 많이 작성한 사용자 분석
+drop procedure if exists get_top_user_by_cnt;
+delimiter //
+create procedure get_top_user_by_cnt(
+    in p_tableName varchar(20),
+    in p_colCount varchar(20)
+)
+begin
+    -- 안전한 컬럼 및 테이블 검증 (comment와 question 테이블만 허용)
+    if p_tableName not in ('comment', 'question') then
+        signal sqlstate '45000' set message_text = 'Invalid table name';
+    elseif p_colCount not in ('cid', 'qid') then
+        signal sqlstate '45000' set message_text = 'Invalid column name';
+end if;
+
+    -- 동적 쿼리 생성
+    set @query = concat(
+            'select u.uid, u.name, u.company, count(*) as cnt ',
+            'from ', p_tableName, ' as q ',
+            'join user as u on q.uid = u.uid ',
+            'group by u.uid ',
+        -- 질문 갯수 기준으로 내림차순 정렬
+            'order by cnt desc'
+                 );
+
+    -- 동적 쿼리 실행
+prepare stmt from @query;
+execute stmt;
+deallocate prepare stmt;
+end //
+delimiter ;
+
+-- 특정 회사 내 최다 질문 또는 코멘트를 작성한 사용자
+-- 회사 이름과 질문 or 코멘트 테이블을 입력받아 가장 많이 작성한 회사를 내림차순으로 출력
+drop procedure if exists get_top_user_company_by_cnt;
+delimiter //
+create procedure get_top_user_company_by_cnt(
+    in p_company varchar(10),
+    in p_tableName varchar(20)
+)
+begin
+    set @query = concat(
+            'select u.company, count(*) as ', p_tableName, '_cnt ',
+            'from ', p_tableName, ' as t ',
+            'join user as u on t.uid = u.uid ',
+            'where u.company = ''', p_company, ''' ',
+            'group by u.company ',
+            'order by ', p_tableName, '_cnt desc'
+                 );
 end //
 delimiter ;
 
@@ -400,27 +445,28 @@ drop procedure if exists get_popular_topics;
 delimiter //
 create procedure get_popular_topics()
 begin
-    select t.tname as topic, count(*) as question_count
-    from question as q
-             join question_tag as qt on q.qid = qt.qid
-             join tag as t on qt.tid = t.tid
-    group by t.tname
-    order by question_count desc
+select t.tname as topic, count(*) as question_count
+from question as q
+         join question_tag as qt on q.qid = qt.qid
+         join tag as t on qt.tid = t.tid
+group by t.tname
+order by question_count desc
     limit 10;
 end //
 delimiter ;
-call get_popular_topics();
 
 -- 소속 회사별 활동 현황 분석
 drop procedure if exists get_activity_by_company;
 delimiter //
 create procedure get_activity_by_company()
 begin
-    select u.company, count(q.qid) as question_cnt, count(c.cid) as comment_cnt
-    from user as u
-             join question as q on u.uid = q.uid
-             join comment as c on u.uid = c.uid
-    group by u.company;
+select u.company,
+       count(distinct q.qid) as question_cnt,
+       count(distinct c.cid) as comment_cnt
+from user as u
+         left join question as q on u.uid = q.uid
+         left join comment as c on u.uid = c.uid
+group by u.company;
 end //
 delimiter ;
 
@@ -429,10 +475,10 @@ drop procedure if exists get_activity_by_time;
 delimiter //
 create procedure get_activity_by_time()
 begin
-    select hour(createAt) as hour, count(*) as activity_cnt
-    from question
-    group by hour
-    order by hour;
+select hour(createAt) as hour, count(*) as activity_cnt
+from question
+group by hour
+order by hour;
 end //
 delimiter ;
 
@@ -441,10 +487,10 @@ drop procedure if exists get_user_response_time;
 delimiter //
 create procedure get_user_response_time(in p_uid int)
 begin
-    select q.qid, timestampdiff(minute, q.createAt, min(c.createAt)) as response_time_minutes
-    from question as q
-             join comment as c on q.qid = c.qid
-    where q.uid = p_uid
-    group by q.qid;
+select q.qid, timestampdiff(minute, q.createAt, min(c.createAt)) as response_time_minutes
+from question as q
+         join comment as c on q.qid = c.qid
+where q.uid = p_uid
+group by q.qid;
 end //
 delimiter ;
